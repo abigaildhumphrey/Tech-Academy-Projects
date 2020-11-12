a58fef8d0f18142776555b2a6b1bb2113f784e74
abigaildhumphrey <abbyhumphrey@me.com>
2020-11-11 23:37:13 -0800

    Update

---
+++ Portfolio/JavaScript/javascript.js
@@ -0,0 +1,9 @@
+
+//functions for the open and closing of contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
